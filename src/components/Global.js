3878847c5cd6319e1e0fa05538c88a42fa819d9f
Roba Nath Basnet <robanbth@gmail.com>
2022-10-11 02:30:49 +0600

    Completed the API integration and styling

--- src/components/Global.js
+++ src/components/Global.js
@@ -2,21 +2,29 @@ import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Stack from "react-bootstrap/Stack";
+import Gallery from "./Gallery";
 
 class Global extends Component {
   constructor(props) {
     super(props);
     this.state = {
       query: "",
+      items: [],
     };
   }
 
   search() {
-    console.log("Search", this.state.query);
+    const BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+    fetch(`${BASE_URL}${this.state.query}`, { method: "GET" })
+      .then((response) => response.json())
+      .then((json) => {
+        let { items } = json;
+        this.setState({ items });
+      });
   }
   render() {
     return (
-      <div className="p-3">
+      <div className=" Global">
         <h2 className="text-center fw-bold">Book Explorer</h2>
         <Stack direction="horizontal" gap={3}>
           <Form.Control
@@ -33,6 +41,7 @@ class Global extends Component {
             Search
           </Button>
         </Stack>
+        <Gallery items={this.state.items} />
       </div>
     );
   }
